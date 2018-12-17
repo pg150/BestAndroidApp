3c3c06a9b70ae73a095ad32debdcc6643a5064e2
Issa ROMPION <irompion@yahoo.fr>
2018-12-17 17:09:07 +0100

    ajout du p2p

--- v1NotreAppli/app/src/main/java/com/example/issarompion/v1notreappli/TwoPlayersActivity.java
+++ v1NotreAppli/app/src/main/java/com/example/issarompion/v1notreappli/TwoPlayersActivity.java
@@ -1,13 +1,38 @@
 package com.example.issarompion.v1notreappli;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TwoPlayersActivity extends AppCompatActivity {
+    private Button create;
+    private Button join;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two_players);
+        create = (Button) findViewById(R.id.create);
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openP2PActivity();
+            }
+        });
+        join = (Button) findViewById(R.id.join);
+        join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openP2PActivity();
+            }
+        });
     }
+
+    public void openP2PActivity() {
+        Intent intent = new Intent(this, p2p.class);
+        startActivity(intent);
+    }
+
 }
---
+++ v1NotreAppli/app/src/main/java/com/example/issarompion/v1notreappli/p2p.java
@@ -0,0 +1,163 @@
+package com.example.issarompion.v1notreappli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.net.wifi.WifiManager;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class p2p extends AppCompatActivity {
+
+    Button btnOnOff, btnDiscover, btnPlay;
+    ListView listView;
+    TextView read_msg_box, connectionStatus;
+    EditText writeMsg;
+
+    WifiManager  wifiManager;
+    WifiP2pManager mManager;
+    WifiP2pManager.Channel mChannel;
+
+    BroadcastReceiver mReceiver;
+    IntentFilter mIntentFilter;
+
+    List<WifiP2pDevice> peers=new ArrayList<WifiP2pDevice>();
+    String[] deviceNameArray;
+    WifiP2pDevice[] deviceArray;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p2p);
+        initialWork();
+        exqListener();
+    }
+
+    private void exqListener(){
+        btnOnOff.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(wifiManager.isWifiEnabled()){
+                    wifiManager.setWifiEnabled(false);
+                    btnOnOff.setText("WIFI ON");
+                }
+                else{
+                    wifiManager.setWifiEnabled(true);
+                    btnOnOff.setText("WIFI OFF");
+                }
+            }
+
+        });
+
+        btnDiscover.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        connectionStatus.setText("Discovery Started");
+                    }
+
+                    @Override
+                    public void onFailure(int reason) {
+                        connectionStatus.setText("Discrovery Starting Failed");
+                    }
+                });
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final WifiP2pDevice device = deviceArray[position];
+                WifiP2pConfig config = new WifiP2pConfig();
+                config.deviceAddress=device.deviceAddress;
+
+
+            }
+
+        });
+    }
+
+    private void initialWork() {
+        btnOnOff=(Button) findViewById(R.id.onOff);
+        btnDiscover=(Button) findViewById(R.id.discover);
+        btnPlay=(Button) findViewById(R.id.btnPlay);
+        listView=(ListView) findViewById(R.id.peerListView);
+        connectionStatus=(TextView) findViewById(R.id.connectionStatus);
+
+        wifiManager= (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        mManager= (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel= mManager.initialize(this,getMainLooper(),null);
+
+        mReceiver=new com.example.issarompion.v1notreappli.WiFiDirectBroadcastReceiver(mManager,mChannel,this);
+
+        mIntentFilter=new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+    }
+
+   public  WifiP2pManager.PeerListListener peerListListener= new WifiP2pManager.PeerListListener() {
+        @Override
+        public void onPeersAvailable(WifiP2pDeviceList peerList) {
+            if(!peerList.getDeviceList().equals(peers)){
+                Toast.makeText(getApplicationContext(),"On rentre", Toast.LENGTH_SHORT).show();
+                peers.clear();
+                peers.addAll(peerList.getDeviceList());
+
+                deviceNameArray=new String[peerList.getDeviceList().size()];
+                deviceArray=new WifiP2pDevice[peerList.getDeviceList().size()];
+                int index=0;
+
+                for(WifiP2pDevice device : peerList.getDeviceList()){
+                    deviceNameArray[index]=device.deviceName;
+                    deviceArray[index]=device;
+                    index++;
+                }
+                ArrayAdapter<String> adapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,deviceNameArray);
+                listView.setAdapter(adapter);
+            }
+
+            if(peers.size()==0){
+                Toast.makeText(getApplicationContext(),"No Device Found", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver,mIntentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+
+
+}
